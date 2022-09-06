403188dfa14b5b22feb96d47fc27cfa67bef14f6
Anatoliy <anatoliypuss5@gmail.com>
2022-09-06 20:36:47 +0300

    Commit of the first week

---
+++ week01/ex1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ls /usr/bin | grep 'gcc' | sort -r | tail -n 5 > ~/week1/ex1.txt
---
+++ week01/ex2.sh
@@ -0,0 +1,60 @@
+firefox
+git config --global user.name "Anatoliy"
+git config --global user.email "anatoliypuss5@gmail.com"
+ssh-keygen
+cat /home/tolyasek/.ssh/id_rsa
+ssh-add /home/tolyasek/.ssh/id_rsa
+cat /home/tolyasek/.ssh/id_rsa.pub
+ssh -T git@github.com
+pwd
+ls
+cd Documents/
+git clone https://github.com/anatoliypus/inno-os.git
+git clone git@github.com:anatoliypus/inno-os.git
+pwd
+ls
+pwd
+cd ..
+ls
+mkdir week1
+cd week1
+touch ex1.sh
+code ex1.sh
+pwd
+cd week1
+sh ex1.sh
+man grep
+sh ex1.sh
+clear
+sh ex1.sh
+man grep
+sh ex1.sh
+ls
+cat 1.txt
+pwd
+ls /usr/bin
+ls /usr/bin | grep 'gcc'
+sudo apt-get install build-essentials
+sudo apt-get install build-essential
+sudo apt-get install manpages-dev
+gcc --version
+ls /usr/bin
+sh ex1.sh
+man
+man sort
+sh ex1.sh
+whoami
+hostname
+man man
+sh ex2.sh
+sudo apt-get install bash
+chsh -s /bin/bash
+sh ex2.sh
+man cut
+ls
+cd week1
+ls
+rm ex2.sh
+rm ex2.txt
+ls
+history > ex2.txt
